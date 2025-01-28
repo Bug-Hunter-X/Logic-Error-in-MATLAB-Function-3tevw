b58f795facb2cf66edd35706352d4257092204f0
BugHunter-X <askzenai@gmail.com>
2025-01-28 13:26:04 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,14 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  elseif anotherCondition
+    result = anotherValue;
+  else
+    result = defaultValue;
+  end
+end
+
+% Example usage
+input = 10;
+result = myFunction(input); 
